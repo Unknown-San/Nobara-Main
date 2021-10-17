195939646443fb86c0669e69ef442b6b635e0db7
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-10-17 07:17:14 +0530

    Update Wall.py

--- SaitamaRobot/modules/Wall.py
+++ SaitamaRobot/modules/Wall.py
@@ -1,48 +1,57 @@
-from random import choice
-from time import sleep
-from urllib.parse import quote
+from random import randint
 
 import requests as r
-from telegram import Message, Chat, Update, Bot
-from telegram.ext import run_async
-
-from SaitamaRobot import dispatcher, WALL_API
+from SaitamaRobot import SUPPORT_CHAT, WALL_API, dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
+# Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
 
 @run_async
-def wall(bot: Bot, update: Update, args):
+def wall(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     msg = update.effective_message
+    args = context.args
     msg_id = update.effective_message.message_id
+    bot = context.bot
     query = " ".join(args)
     if not query:
         msg.reply_text("Please enter a query!")
         return
     else:
         caption = query
-        term = quote(caption)
-        json_rep = r.get(f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}").json()
+        term = query.replace(" ", "%20")
+        json_rep = r.get(
+            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
+        ).json()
         if not json_rep.get("success"):
-            msg.reply_text("Something went wrong...")
-            return
+            msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
         else:
             wallpapers = json_rep.get("wallpapers")
             if not wallpapers:
-                msg.reply_text("No results found!")
+                msg.reply_text("No results found! Refine your search.")
                 return
             else:
-                wallpaper = choice(wallpapers)
+                index = randint(0, len(wallpapers) - 1)  # Choose random index
+                wallpaper = wallpapers[index]
                 wallpaper = wallpaper.get("url_image")
                 wallpaper = wallpaper.replace("\\", "")
-                bot.send_photo(chat_id, photo=wallpaper, caption='Preview',
-                reply_to_message_id=msg_id, timeout=60)
-                bot.send_document(chat_id, document=wallpaper,
-                filename='wallpaper', caption=caption, reply_to_message_id=msg_id,
-                timeout=60)
-                    
-            
-            
-WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall, pass_args=True)
+                bot.send_photo(
+                    chat_id,
+                    photo=wallpaper,
+                    caption='Preview',
+                    reply_to_message_id=msg_id,
+                    timeout=60)
+                bot.send_document(
+                    chat_id,
+                    document=wallpaper,
+                    filename='wallpaper',
+                    caption=caption,
+                    reply_to_message_id=msg_id,
+                    timeout=60)
 
+
+WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall)
 dispatcher.add_handler(WALLPAPER_HANDLER)
