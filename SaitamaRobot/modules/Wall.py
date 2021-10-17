a59297f47357efcce94cd17147e02ec10ce35522
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-10-17 07:10:53 +0530

    Update Wall.py

--- SaitamaRobot/modules/Wall.py
+++ SaitamaRobot/modules/Wall.py
@@ -1,22 +1,48 @@
-import asyncio
-from aiohttp import ClientSession
-from Python_ARQ import ARQ
-import random
-from pyrogram import filters
-from SaitamaRobot import pbot as bot
+from random import choice
+from time import sleep
+from urllib.parse import quote
 
-api_key = ('RIZMUN-PTUQTE-ZDXUWJ-AGZJVR-ARQ') # get it from @arqrobot
-api_url = "https://thearq.tech"
+import requests as r
+from telegram import Message, Chat, Update, Bot
+from telegram.ext import run_async
+
+from SaitamaRobot import dispatcher, WALL_API
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 
 
-@bot.on_message(filters.command('wall'))
-async def main(_,message):
-    session = ClientSession()
-    wall_ = message.text.replace(message.text.split(' ')[0], '')
-    arq = ARQ(api_url, api_key, session)
-    results = await arq.wall(wall_)
-    wallpaper = results.result
-    x = random.choice(wallpaper)
-    print(x.url_image)
-    await message.reply_photo(x.url_image)
-    await session.close()
+@run_async
+def wall(bot: Bot, update: Update, args):
+    chat_id = update.effective_chat.id
+    msg = update.effective_message
+    msg_id = update.effective_message.message_id
+    query = " ".join(args)
+    if not query:
+        msg.reply_text("Please enter a query!")
+        return
+    else:
+        caption = query
+        term = quote(caption)
+        json_rep = r.get(f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}").json()
+        if not json_rep.get("success"):
+            msg.reply_text("Something went wrong...")
+            return
+        else:
+            wallpapers = json_rep.get("wallpapers")
+            if not wallpapers:
+                msg.reply_text("No results found!")
+                return
+            else:
+                wallpaper = choice(wallpapers)
+                wallpaper = wallpaper.get("url_image")
+                wallpaper = wallpaper.replace("\\", "")
+                bot.send_photo(chat_id, photo=wallpaper, caption='Preview',
+                reply_to_message_id=msg_id, timeout=60)
+                bot.send_document(chat_id, document=wallpaper,
+                filename='wallpaper', caption=caption, reply_to_message_id=msg_id,
+                timeout=60)
+                    
+            
+            
+WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall, pass_args=True)
+
+dispatcher.add_handler(WALLPAPER_HANDLER)
