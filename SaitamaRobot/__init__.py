3f95b2fa229dc8897a79a2a539f8bb0d60d60ece
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-12-31 20:20:34 -0800

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -180,6 +180,15 @@ arq = ARQ("https://thearq.tech", "UIUXOY-NTKWDC-QHFFMD-DHHKVV-ARQ", aiohttpsessi
 def typing(func):
     """Sends typing action while processing func command."""
 
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING
+        )
+        return func(update, context, *args, **kwargs)
+
+    return command_func
+
 if not SPAMWATCH_API:
     sw = None
     LOGGER.warning("SpamWatch API key missing! recheck your config.")
