f30b68680e68966f4e7d3fa51b1a557a9156cd59
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-07 11:28:18 +0530

    Update global_bans.py

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -178,8 +178,17 @@ def gban(update: Update, context: CallbackContext):
         return
     NOBARA="https://telegra.ph/file/827599e5d7a358023ce66.mp4"
     NOARAARF="Ryōiki Tenkai , Welcome to Nobara Domain"
-    message.reply_video(NOBARA , caption=NOARAARF , reply_to_message_id=reply,
-    parse_mode=ParseMode.HTML,
+    message.reply_video(NOBARA , caption=NOARAARF ,
+    reply_markup=InlineKeyboardMarkup(
+                [
+             [
+                InlineKeyboardButton(text="Request to reborn", url="https://t.me/NobaraSupport"),
+             ],
+          ]
+            
+        ),          
+   
+  parse_mode=ParseMode.MARKDOWN
        )
 
     start_time = time.time()
