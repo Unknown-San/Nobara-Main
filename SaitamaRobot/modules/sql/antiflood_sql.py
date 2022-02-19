60d80d75bb5559f2e24155498b9d137cd5f0bdbc
Toman-Mikey <96868350+Toman-Mikey@users.noreply.github.com>
2022-02-19 15:18:10 +0530

    Update antiflood_sql.py

--- SaitamaRobot/modules/sql/antiflood_sql.py
+++ SaitamaRobot/modules/sql/antiflood_sql.py
@@ -3,6 +3,8 @@ import threading
 from sqlalchemy import String, Column, Integer, UnicodeText
 
 from SaitamaRobot.modules.sql import SESSION, BASE
+from sqlalchemy.sql.sqltypes import BigInteger
+
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
@@ -12,7 +14,7 @@ DEF_OBJ = (None, DEF_COUNT, DEF_LIMIT)
 class FloodControl(BASE):
     __tablename__ = "antiflood"
     chat_id = Column(String(14), primary_key=True)
-    user_id = Column(Integer)
+    user_id = Column(BigInteger)
     count = Column(Integer, default=DEF_COUNT)
     limit = Column(Integer, default=DEF_LIMIT)
 
